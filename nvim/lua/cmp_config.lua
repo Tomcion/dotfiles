2dd6d9aa4242c65e03197c4e448234281f38e3be
Tymon <79712082+Tomcion@users.noreply.github.com>
2022-06-17 17:40:37 +0200

    Fixing the nvim cmp

--- nvim/lua/cmp_config.lua
+++ nvim/lua/cmp_config.lua
@@ -10,39 +10,6 @@ end
 
 require("luasnip/loaders/from_vscode").lazy_load()
 
-local has_words_before = function()
-    local col = vim.fn.col "." - 1
-    return col == 0 or vim.fn.getline("."):sub(col, col):match "%s"
-end
-
--- local kind_icons = {
---     Text = "",
---     Method = "m",
---     Function = "",
---     Constructor = "",
---     Field = "",
---     Variable = "",
---     Class = "",
---     Interface = "",
---     Module = "",
---     Property = "",
---     Unit = "",
---     Value = "",
---     Enum = "",
---     Keyword = "",
---     Snippet = "",
---     Color = "",
---     File = "",
---     Reference = "",
---     Folder = "",
---     EnumMember = "",
---     Constant = "",
---     Struct = "",
---     Event = "",
---     Operator = "",
---     TypeParameter = "",
--- }
-
 local kind_icons = {
     Text = '  ',
     Method = '  ',
@@ -79,30 +46,25 @@ cmp.setup {
     },
 
     mapping = {
-        ["<C-k>"] = cmp.mapping.select_prev_item(),
-        ["<C-j>"] = cmp.mapping.select_next_item(),
+        ["<C-p>"] = cmp.mapping.select_prev_item(),
+        ["<C-n>"] = cmp.mapping.select_next_item(),
         ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
         ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
         ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
-        ["<C-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
         ["<C-e>"] = cmp.mapping {
             i = cmp.mapping.abort(),
             c = cmp.mapping.close(),
         },
-        -- Accept currently selected item. If none selected, `select` first item.
-        -- Set `select` to `false` to only confirm explicitly selected items.
-        ["<CR>"] = cmp.mapping.confirm { select = true },
-        ["<Tab>"] = cmp.mapping(function(fallback)
+        ["<CR>"] = cmp.mapping.confirm { select = false },
+        ['<Tab>'] = cmp.mapping(function(fallback)
             if cmp.visible() then
                 cmp.select_next_item()
             elseif luasnip.expand_or_jumpable() then
-                luasnip.expand_or_jump()
-            elseif has_words_before() then
-                cmp.complete()
+                luasnip.expand_of_jump()
             else
                 fallback()
             end
-        end, { "i", "s", }),
+        end, {'i', 's'}),
 
         ["<S-Tab>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
@@ -118,8 +80,6 @@ cmp.setup {
     formatting = {
         fields = { "kind", "abbr", "menu" },
         format = function(entry, vim_item)
-            -- Kind icons
-            --vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
             vim_item.kind = kind_icons[vim_item.kind]
             vim_item.menu = ({
                 nvim_lsp = "[LSP]",
